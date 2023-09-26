fec8ebc639a1fbebb3a791a1d8dfcbbe75de7cff
Jasemonu <jasemonu@gmail.com>
2023-09-26 07:11:08 -0700

    101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,5 @@
-export default function iterateThroughObject(reportWithIterator) {
-  const employeeNames = [];
-
-  for (const department in reportWithIterator.allEmployees) {
-    if (reportWithIterator.allEmployees.hasOwnProperty(department)) {
-      const employeesInDepartment = reportWithIterator.allEmployees[department];
-      employeeNames.push(...employeesInDepartment);
-    }
-  }
-
-  return employeeNames.join('|');
+export default function createEmployeesObject(departmentName, employees) {
+  return {
+    [departmentName]: [...employees],
+  };
 }
