cc07ec95a45f1248f968c85fcb44de31fa1ffd65
Jasemonu <jasemonu@gmail.com>
2023-10-03 00:16:11 +0000

    code modified

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 export default function getStudentsByLocation(studentList, city) {
   // Use the filter function to find students located in the specified city
-  const filteredStudents = studentList.filter(student => student.location === city);
+  const filteredStudents = studentList.filter((student) => student.location === city);
   return filteredStudents;
 }
