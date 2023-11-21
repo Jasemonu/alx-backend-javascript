9edb0bbfd3a8375211a817af749729e9c26f522d
Jasemonu <jasemonu@gmail.com>
2023-11-21 15:35:51 +0000

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+if (process.isTTY) {
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.exit(0);
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.exit(0);
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
